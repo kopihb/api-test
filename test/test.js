87757abfaf67cff5f13fab711555b011bddee7c7
kopihb <kopihb@gmail.com>
2018-06-26 16:46:32 +0300

    added global variable

--- test/test.js
+++ test/test.js
@@ -3,93 +3,123 @@ var should = require('chai').should(),
     supertest = require('supertest'),
     api = supertest('http://159.100.241.121:5002');
 
+/*Global variable
+*
+* Change the settings for a new pass test
+*
+* */
+var clinic = {
+    name: 'globalTest',
+    latitude: 0,
+    longitude: 0,
+    confirmed: true
+}
 
-describe('Clinics GET request', function () {
-    it('Should return 200 responce', function (done) {
+var consumer = {
+    email: "www@mail.com",
+    name: "my@www.test",
+    phone: "34534534",
+    receiveNotification: true,
+    dontSentAdv: true,
+    signedUp: true,
+    entityStart: "2018-01-01T00:00:00.000Z",
+    entityEnd: "2018-01-01T00:00:00.000Z"
+}
+/*end global variable*/
+
+describe('GET request', function () {
+    it('Should return 200 responce - Clinic', function (done) {
         api.get('/clinics')
             .set('Accept', 'aplication/json')
-            .expect(200,done);
+            //.expect(200,done);
+            .end(function(err, res) {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body).to.be.an('array');
+                done();
+            });
+    });
+    it('Should return 200 responce - Consumers', function (done) {
+        api.get('/consumers')
+            .set('Accept', 'aplication/json')
+            .expect(200,done)
+    });
+    it('Should return 200 responce - Providers', function (done) {
+        api.get('/providers')
+            .set('Accept', 'aplication/json')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
     });
 })
 
 
 describe('Clinics create', function () {
+
     it('Create new clinic/Successfull case', function(done) {
         api.post('/clinics')
             .set('Accept', 'aplication/json')
-            .send({
-                name: 'myClinic2',
-                latitude: 0,
-                longitude: 0,
-                confirmed: true
-            })
+            .send(clinic)
             .expect(200,done)
+
     });
 
     it('Create new clinic/with several words', function(done) {
         api.post('/clinics')
             .set('Accept', 'aplication/json')
-            .send({
-                name: 'my Clinic several words 2',
-                latitude: 0,
-                longitude: 0,
-                confirmed: true
+            .send( {
+                name : clinic.name + 'several',
+                latitude : clinic.latitude,
+                longitude: clinic.longitude,
+                confirmed: clinic.confirmed
             })
             .expect(200,done)
-    });
+        });
 
     it('Create new clinic/name parameter with numbers', function(done) {
         api.post('/clinics')
             .set('Accept', 'aplication/json')
-            .send({
-                name: 'myClinic 12345 2',
-                latitude: 0,
-                longitude: 0,
-                confirmed: true
+            .send( {
+                name : clinic.name + 123658,
+                latitude : clinic.latitude,
+                longitude: clinic.longitude,
+                confirmed: clinic.confirmed
             })
             .expect(200,done)
     });
 
-    it('Create new clinic/with the same title', function(done) {
+    it('Create new clinic/with empty title', function(done) {
         api.post('/clinics')
             .set('Accept', 'aplication/json')
-            .send({
-                name: 'myClinic !#$%#$',
-                latitude: 0,
-                longitude: 0,
-                confirmed: true
+            .send( {
+                name : '',
+                latitude : clinic.latitude,
+                longitude: clinic.longitude,
+                confirmed: clinic.confirmed
             })
             .expect(200,done)
     });
 
-    it('Create new clinic/with  dublicate ', function(done) {
+    it('Create new clinic/with  dublicate', function(done) {
         api.post('/clinics')
             .set('Accept', 'aplication/json')
-            .send({
-                name: 'myClinic !#$%#$',
-                latitude: 0,
-                longitude: 0,
-                confirmed: true
-            })
+            .send(clinic)
             .expect(200,done)
     });
 })
 
 describe('Consumer create', function () {
+
     it('Create new consumer', function(done) {
         api.post('/consumers')
             .set('Accept', 'aplication/json')
-            .send({
-                email: "string",
-                name: "string",
-                phone: "string",
-                receiveNotification: true,
-                dontSentAdv: true,
-                signedUp: true,
-                entityStart: "2018-01-01T00:00:00.000Z",
-                entityEnd: "2018-01-01T00:00:00.000Z"
-            })
+            .send(consumer)
             .expect(200,done)
+            // .end(function(err, res) {
+            //     expect(res.body.name).to.equal(consumer.name);
+            //     done();
+            // });
     });
 
 })
@@ -109,20 +139,3 @@ describe('Consumer check', function () {
 
 })
 
-
-
-describe('TEST POST CLINIC', function () {
-
-
-    it('Create new clinic/with the same title', function(done) {
-        api.post('/clinics')
-        //.set('Accept', 'aplication/json')
-            .send({
-                name: 'myClinics !#$%#$',
-                latitude: 0,
-                longitude: 0,
-                confirmed: true
-            })
-            .expect(200,done)
-    });
-})
