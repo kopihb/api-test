97eba31c7759a690aad66ca94ae77f6769d29444
dmytro.naumenko <dmytro.naumenko89@gmail.com>
2018-06-26 10:31:07 +0300

    Create project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # api-test
+
+
+1 Install project - npm install
+
+2 - To run the test you need to type in the console : npm test
---
+++ test/test.js
@@ -0,0 +1,129 @@
+var should = require('chai').should(),
+    expect = require('chai').expect,
+    supertest = require('supertest'),
+    api = supertest('http://159.100.241.121:5002');
+
+
+describe('Clinics GET', function () {
+    it('Should return 200 responce', function (done) {
+        api.get('/clinics')
+            .set('Accept', 'aplication/json')
+            .expect(200,done);
+    });
+})
+
+
+
+describe('Clinics create', function () {
+    it('Create new clinic/Successfull case', function(done) {
+        api.get('/clinics')
+            .set('Accept', 'aplication/json')
+            .send({
+                name: 'myClinic',
+                latitude: 0,
+                longitude: 0,
+                confirmed: true
+            })
+            //.set('Accept', 'application/json')
+            .expect(200,done)
+    });
+
+    it('Create new clinic/with several words', function(done) {
+        api.get('/clinics')
+            .set('Accept', 'aplication/json')
+            .send({
+                name: 'my Clinic several words',
+                latitude: 0,
+                longitude: 0,
+                confirmed: true
+            })
+            //.set('Accept', 'application/json')
+            .expect(200,done)
+    });
+
+    it('Create new clinic/name parameter with numbers', function(done) {
+        api.get('/clinics')
+            .set('Accept', 'aplication/json')
+            .send({
+                name: 'myClinic 12345',
+                latitude: 0,
+                longitude: 0,
+                confirmed: true
+            })
+            //.set('Accept', 'application/json')
+            .expect(200,done)
+    });
+
+    it('Create new clinic/with the same title', function(done) {
+        api.get('/clinics')
+            .set('Accept', 'aplication/json')
+            .send({
+                name: 'myClinic !#$%#$',
+                latitude: 0,
+                longitude: 0,
+                confirmed: true
+            })
+            //.set('Accept', 'application/json')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
+    it('Create new clinic/with  dublicate ', function(done) {
+        api.get('/clinics')
+            .set('Accept', 'aplication/json')
+            .send({
+                name: 'myClinic !#$%#$',
+                latitude: 0,
+                longitude: 0,
+                confirmed: true
+            })
+            .send({
+                name: 'myClinic !#$%#$',
+                latitude: 0,
+                longitude: 0,
+                confirmed: true
+            })
+            //.set('Accept', 'application/json')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
+})
+
+describe('Consumer create', function () {
+    it('Create new consumer', function(done) {
+        api.get('/consumers')
+            .set('Accept', 'aplication/json')
+            .send({
+                email: "string",
+                name: "string",
+                phone: "string",
+                receiveNotification: true,
+                dontSentAdv: true,
+                signedUp: true,
+                entityStart: "2018-01-01T00:00:00.000Z",
+                entityEnd: "2018-01-01T00:00:00.000Z"
+            })
+            .expect(200,done)
+    });
+
+})
+
+
+
+describe('Consumer create', function () {
+    it('Check the name of the clinic for the ID number', function(done) {
+        api.get('/clinics/5b23cbb99df54e58680a71a9')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function(err, res) {
+                expect(res.body.name).to.equal("new clinic");
+                done();
+            });
+    });
+
+})
