9f198432a9524d81959b668f3e5809079cbbe45c
kopihb <kopihb@gmail.com>
2018-06-27 14:24:43 +0300

    modified new test structure

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ var should = require('chai').should(),
 *
 */
 var clinic = {
-    name: 'Final',
+    name: 'newsss',
     latitude: 0,
     longitude: 0,
     confirmed: true
@@ -27,233 +27,262 @@ var consumer = {
 }
 /*end global variable*/
 
-describe('GET request', function () {
-    it('Should return 200 responce - /clinics', function (done) {
-        api.get('/clinics')
-            .set('Accept', 'aplication/json')
-            //.expect(200,done);
-            .end(function(err, res) {
-                expect(res.statusCode).to.equal(200);
-                expect(res.body).to.be.an('array');
-                done();
-            });
-    });
-    it('Should return 200 responce - /consumers', function (done) {
-        api.get('/consumers')
-            .set('Accept', 'aplication/json')
-            .expect(200,done)
-    });
-    it('Should return 200 responce - /providers', function (done) {
-        api.get('/providers')
-            .set('Accept', 'aplication/json')
-            .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err);
-                done();
-            });
-    });
-})
 
 
-describe('Clinics create', function () {
 
-    it('Create new clinic/Successfull case', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(clinic)
-            .expect(200,done)
+describe('Create clinic', function () {
 
-    });
 
-    it('Create new clinic/with several words', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                name : clinic.name + 'several',
-                latitude : clinic.latitude,
-                longitude: clinic.longitude,
-                confirmed: clinic.confirmed
-            })
-            .expect(200,done)
+    describe('HTTP responce code - 200 ', function () {
+
+        it('Create new clinic/Successfull case', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(clinic)
+                .expect(200,done)
+
         });
 
-    it('Create new clinic/name parameter with numbers', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                name : clinic.name + 123658,
-                latitude : clinic.latitude,
-                longitude: clinic.longitude,
-                confirmed: clinic.confirmed
-            })
-            .expect(200,done)
-    });
+        it('Create new clinic/with several words', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'several',
+                        latitude : clinic.latitude,
+                        longitude: clinic.longitude,
+                        confirmed: clinic.confirmed
+                    })
+                .expect(200,done)
+        });
 
-    it('Create new clinic/with empty title', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                name : '',
-                latitude : clinic.latitude,
-                longitude: clinic.longitude,
-                confirmed: clinic.confirmed
-            })
-            .expect(200,done)
-    });
+        it('Create new clinic/name parameter with numbers', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 123658,
+                        latitude : clinic.latitude,
+                        longitude: clinic.longitude,
+                        confirmed: clinic.confirmed
+                    })
+                .expect(200,done)
+        });
 
-    it('Create new clinic/with missed name parameter', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                latitude : clinic.latitude,
-                longitude: clinic.longitude,
-                confirmed: clinic.confirmed
-            })
-            .expect(200,done)
-    });
+        it('Create new clinic/name parameter with symbols', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + '!%$_^',
+                        latitude : clinic.latitude,
+                        longitude: clinic.longitude,
+                        confirmed: clinic.confirmed
+                    })
+                .expect(200,done)
+        });
 
-    it('Create new clinic/with  dublicate', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(clinic)
-            .expect(200,done)
-    });
+        it('Create new clinic/confirmed "false', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'word',
+                        latitude : clinic.latitude,
+                        longitude: clinic.longitude,
+                        confirmed: false
+                    })
+                .expect(200,done)
+        });
 
-    it('Create new clinic/incorrect format for latitude', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                    name : clinic.name + 'word',
-                    latitude : '',
-                    longitude: clinic.longitude,
-                    confirmed: clinic.confirmed
-                })
-            .expect(200,done)
-    });
+    })
 
-    it('Create new clinic/with missed longitude', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                    name : clinic.name + 'word',
-                    latitude : clinic.latitude,
-                    longitude: '',
-                    confirmed: clinic.confirmed
-                })
-            .expect(200,done)
-    });
 
-    it('Create new clinic/incorrect format for title/number instead of string', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                    name : 0,
-                    latitude : clinic.latitude,
-                    longitude: clinic.longitude,
-                    confirmed: clinic.confirmed
-                })
-            .expect(200,done)
-    });
 
-    it('Create new clinic/incorrect format for name parameter/null', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                    name : null,
-                    latitude : clinic.latitude,
-                    longitude: clinic.longitude,
-                    confirmed: clinic.confirmed
-                })
-            .expect(200,done)
-    });
+    describe('HTTP responce code - 400', function () {
 
-    it('Create new clinic/confirmed "false', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                    name : clinic.name + 'word',
-                    latitude : clinic.latitude,
-                    longitude: clinic.longitude,
-                    confirmed: false
-                })
-            .expect(200,done)
-    });
+        it('Create new clinic/with the same title', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(clinic)
+                .expect(400,done)
+        });
 
-    it('Create new clinic/confirmed - validation for parameter type number', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                    name : clinic.name + 'word',
-                    latitude : clinic.latitude,
-                    longitude: clinic.longitude,
-                    confirmed: 0
-                })
-            .expect(200,done)
-    });
+        it('Create new clinic/with empty title', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : '',
+                        latitude : clinic.latitude,
+                        longitude: clinic.longitude,
+                        confirmed: clinic.confirmed
+                    })
+                .expect(400,done)
+        });
 
-    it('Create new clinic/confirmed - validation for parameter type text', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                    name : clinic.name + 'word',
-                    latitude : clinic.latitude,
-                    longitude: clinic.longitude,
-                    confirmed: 'text'
-                })
-            .expect(200,done)
-    });
+        it('Create new clinic/with missed name parameter', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        latitude : clinic.latitude,
+                        longitude: clinic.longitude,
+                        confirmed: clinic.confirmed
+                    })
+                .expect(400,done)
+        });
 
-    it('Create new clinic/latitude - null instead of num', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                    name : clinic.name + 'word',
-                    latitude : null,
-                    longitude: 0,
-                    confirmed: true
-                })
-            .expect(200,done)
-    });
+        it('Create new clinic/incorrect format for latitude', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'word',
+                        longitude: clinic.longitude,
+                        confirmed: clinic.confirmed
+                    })
+                .expect(400,done)
+        });
 
-    it('Create new clinic/longitude - null instead of num', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                    name : clinic.name + 'word',
-                    latitude : 0,
-                    longitude: null,
-                    confirmed: true
-                })
-            .expect(200,done)
-    });
+        it('Create new clinic/with missed longitude', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'word',
+                        latitude : clinic.latitude,
+                        confirmed: clinic.confirmed
+                    })
+                .expect(400,done)
+        });
 
-    it('Create new clinic/"confirmed" - null instead of boolean', function(done) {
-        api.post('/clinics')
-            .set('Accept', 'aplication/json')
-            .send(
-                {
-                    name : clinic.name + 'word',
-                    latitude : 0,
-                    longitude: 0,
-                    confirmed: true
-                })
-            .expect(200,done)
-    });
+        it('Create new clinic/incorrect format for title/number instead of string', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : 0,
+                        latitude : clinic.latitude,
+                        longitude: clinic.longitude,
+                        confirmed: clinic.confirmed
+                    })
+                .expect(400,done)
+        });
 
+        it('Create new clinic/incorrect format for name parameter/null', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : null,
+                        latitude : clinic.latitude,
+                        longitude: clinic.longitude,
+                        confirmed: clinic.confirmed
+                    })
+                .expect(400,done)
+        });
+
+
+
+        it('Create new clinic/confirmed - validation for parameter type number', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'word',
+                        latitude : clinic.latitude,
+                        longitude: clinic.longitude,
+                        confirmed: 0
+                    })
+                .expect(400,done)
+        });
+
+
+        it('Create new clinic/confirmed - validation for parameter type text', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'word',
+                        latitude : clinic.latitude,
+                        longitude: clinic.longitude,
+                        confirmed: 'text'
+                    })
+                .expect(400,done)
+        });
+
+        it('Create new clinic/latitude - null instead of num', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'word',
+                        latitude : null,
+                        longitude: 0,
+                        confirmed: true
+                    })
+                .expect(400,done)
+        });
+
+        it('Create new clinic/longitude - null instead of num', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'word',
+                        latitude : 0,
+                        longitude: null,
+                        confirmed: true
+                    })
+                .expect(400,done)
+        });
+
+        it('Create new clinic/"confirmed" - null instead of boolean', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'word',
+                        latitude : 0,
+                        longitude: 0,
+                        confirmed: null
+                    })
+                .expect(400,done)
+        });
+
+        it('Create new clinic/incorrect format for latitude', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'word',
+                        latitude : '',
+                        longitude: 0,
+                        confirmed: true
+                    })
+                .expect(400,done)
+        });
+
+        it('Create new clinic/incorrect format for longitude', function(done) {
+            api.post('/clinics')
+                .set('Accept', 'aplication/json')
+                .send(
+                    {
+                        name : clinic.name + 'word',
+                        latitude : 0,
+                        longitude: '',
+                        confirmed: true
+                    })
+                .expect(400,done)
+        });
+    })
 })
+
+
+/*old in down*/
+
+
 
 describe('Consumer create', function () {
 
@@ -285,3 +314,29 @@ describe('Consumer check', function () {
 
 })
 
+describe('GET request', function () {
+    it('Should return 200 responce - /clinics', function (done) {
+        api.get('/clinics')
+            .set('Accept', 'aplication/json')
+            //.expect(200,done);
+            .end(function(err, res) {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body).to.be.an('array');
+                done();
+            });
+    });
+    it('Should return 200 responce - /consumers', function (done) {
+        api.get('/consumers')
+            .set('Accept', 'aplication/json')
+            .expect(200,done)
+    });
+    it('Should return 200 responce - /providers', function (done) {
+        api.get('/providers')
+            .set('Accept', 'aplication/json')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
+})
